0accea7324542192a6a25fc768f1f11bfc0dd88e
CodeMarcos <code.henrique@gmail.com>
2022-11-17 12:23:19 -0300

    First Version

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['employees'] = Employee::orderBy('id','desc')->paginate(5);
+        return view('employee.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('employee.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+        'name' => 'required',
+        'charge' => 'required'
+        ]);
+        $employee = new Employee;
+        $employee->name = $request->name;
+        $employee->charge = $request->charge;
+        $employee->save();
+        return redirect()->route('employee.index')
+        ->with('success','New Employee has been registered successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Employee $employee)
+    {
+        return view('employee.show',compact('employee'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Employee $employee)
+    {
+        return view('employee.edit',compact('employee'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Employee $employee)
+    {
+        $request->validate([
+        'name' => 'required',
+        'charge' => 'required'
+        ]);
+        $employees = Employee::find($id);
+        $employees->name = $request->name;
+        $employees->charge = $request->charge;
+        $employees->save();
+        return redirect()->route('employee.index')
+        ->with('success','Employee Has Been updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Employee $employee)
+    {
+        $employee->delete();
+        return redirect()->route('employee.index')
+        ->with('success','Employee has been deleted successfully');
+    }
+}
